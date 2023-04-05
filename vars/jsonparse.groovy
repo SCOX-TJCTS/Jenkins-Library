dd108e82e7d10d39176a71bc2181585093bddd1f
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-05 10:02:34 -0400

    Update jsonparse.groovy

--- vars/jsonparse.groovy
+++ vars/jsonparse.groovy
@@ -1,7 +1,9 @@
+import groovy.json.JsonSlurperClassic
+
 def call() {
 
     def raw = libraryResource 'configuration.json'
-    def json = readJSON file: (raw)
+    def json = new JsonSlurperClassic().parseText(raw)
     def job = json.get(env.JOB_NAME)
 
   println job.profile
