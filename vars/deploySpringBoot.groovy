f3f8b6c58752af7e7ee246b63c61157f70bcdddd
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-05 10:40:22 -0400

    Update deploySpringBoot.groovy

--- vars/deploySpringBoot.groovy
+++ vars/deploySpringBoot.groovy
@@ -7,6 +7,6 @@ def call(Map props = [:]) {
     def job = json.get(env.JOB_NAME)
 
     echo "Deploying artifact for ${job.profile}"
-    echo "Deploying to server: ${prop.targets}"
+    echo "Deploying to server: ${job.targets}"
     
     }
