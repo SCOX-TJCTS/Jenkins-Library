401ae66f4bba414736dd102532062ac3eb7227f0
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-06 08:10:54 -0400

    Update deploySpringBoot.groovy

--- vars/deploySpringBoot.groovy
+++ vars/deploySpringBoot.groovy
@@ -1,12 +1,5 @@
-import groovy.json.JsonSlurperClassic
-
 def call(Map props = [:]) {
-
-    def raw = libraryResource 'configuration.json'
-    def json = new JsonSlurperClassic().parseText(raw)
-    def job = json.get(env.JOB_NAME)
-
-    echo "Deploying artifact for ${job.profile}"
+    echo "Deploying artifact for ${props.profile}"
     echo "Deploying to server: ${props.target}"
     
     }
