9812b667bceec547a439c4a4e9b130f12ba7da14
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-06 08:17:03 -0400

    Update deploySpringBoot.groovy

--- vars/deploySpringBoot.groovy
+++ vars/deploySpringBoot.groovy
@@ -1,4 +1,7 @@
 def call(Map props = [:]) {
+    props = [ profile: "test"] << props
+    
+    
     echo "Deploying artifact for ${props.profile}"
     echo "Deploying to server: ${props.target}"
     
