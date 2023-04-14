3ccc8d287c8f235178635e1cb2963a985609ee57
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-14 08:42:47 -0400

    Update deploySpringBootLegacy.groovy

--- vars/deploySpringBootLegacy.groovy
+++ vars/deploySpringBootLegacy.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonSlurperClassic
 
 def call(Map props = [:]) {
     
-def raw = libraryResource 'job-configuration.json'
+def raw = libraryResource 'configuration.json'
 def json = new JsonSlurperClassic().parseText(raw)
 def job = json.get(env.JOB_NAME)
     
