b7d8c486f12e1fbf7d321ce0ff4f2196b561f823
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-19 14:49:22 -0400

    Update deploySpringBoot.groovy

--- vars/deploySpringBoot.groovy
+++ vars/deploySpringBoot.groovy
@@ -1,8 +1,9 @@
 def call(Map props = [:]) {
-    props = [ profile: "test", artifact:"ROOT.jar"] << props
+    props = [ profile: "test", artifact:"ROOT.jar", appName:"springBoot-app"] << props
     
     
     echo "Deploying artifact for ${props.profile}"
     echo "Deploying to server: ${props.target}"
     echo "The artifact is named: ${props.artifact}"
+    echo "The application name is ${props.appName}"
     }
