6cf902dda4cc03f203f80562c604f97a72f92f3a
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-20 12:51:32 -0400

    Update deploySpringBoot.groovy

--- vars/deploySpringBoot.groovy
+++ vars/deploySpringBoot.groovy
@@ -1,5 +1,5 @@
 def call(Map props = [:]) {
-    props = [ profile: "test", appName: "springBoot-app", artifact: 'ROOT'] << props
+    props = [ profile: "test", appName: "springBoot-app", artifact: "ROOT"] << props
     
   
     
