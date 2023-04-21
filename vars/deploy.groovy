072892ee5dbd7d9f4499a2af208f3ec6043c81d2
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-21 08:26:03 -0400

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,29 @@
+import groovy.json.JsonSlurperClassic
+
+def call(Map props = [:]) {
+	def raw = libraryResource 'configuration.json'
+	def json = new JsonSlurperClassic().parseText(raw)
+	def job = json.get(env.JOB_NAME)
+
+
+	job.targets.each { target ->
+
+		if (target.f5 != null) {
+			f5Down(target: target.f5)
+		}
+
+		if (job.type == 'SPRING_BOOT_LEGACY') {
+			deploySpringBootLegacy(target: target.ssh, profile: job.profile)
+		}
+		else if (job.type == 'SPRING_BOOT') {
+			deploySpringBoot(target: target.ssh, profile: job.profile)
+		}
+		else if (job.type == 'TOMCAT') {
+			deployTomcat(target: target.ssh, profile: job.profile)
+		}
+
+		if (target.f5 != null) {
+			f5Up(target: target.f5)
+		}
+	}
+}
