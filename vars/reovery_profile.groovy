1d8e91a18cdf7236b8f12d1d720e5c9e9977bf3e
SCOX-TJCTS <124071719+SCOX-TJCTS@users.noreply.github.com>
2023-04-24 09:45:41 -0400

    Update reovery_profile.groovy

--- vars/reovery_profile.groovy
+++ vars/reovery_profile.groovy
@@ -1,12 +1,6 @@
-import groovy.json.JsonSlurperClassic
-
 def call(Map props = [:]) {
-	def raw = libraryResource 'configuration.json'
-	def json = new JsonSlurperClassic().parseText(raw)
-	def job = json.get(env.JOB_NAME)
-
-
-	if (job.recovery != null) {
-			build job: job.recovery, wait: true
+	
+	if (props.recovery != null) {
+			build job: props.recovery, wait: true
 		}
 }
